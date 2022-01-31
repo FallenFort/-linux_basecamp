bf309669048626b4eb0b0ed8352b8ed893efb10a
Ovod <olegslavitski@gmail.com>
2022-01-31 20:44:33 +0200

    first commit

---
+++ show_user_info_and_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bas
+
+username=$(whoami)
+DIR_PATH=$(pwd)
+
+#I can't understand how to put the text in the end of string... Maybe I need to do this with loop func?
+echo "$(find $DIR_PATH -user $username -exec echo 'is found!' {} \;)"
+
+if [ $username == olek ]; then
+	echo "$(ps -o pid,user,cmd -u $username)"
+else
+	echo 'Finding “root” user data is not allowed!'
+	exit 42
+fi
